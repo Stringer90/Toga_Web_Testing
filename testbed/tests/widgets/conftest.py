19ea5c7290e47b64b22f9acb84999fb9043c3683
vt37 <110211722+vt37@users.noreply.github.com>
2025-08-28 06:15:21 +0800

    Added conftest and get_probe

--- testbed/tests/widgets/conftest.py
+++ testbed/tests/widgets/conftest.py
@@ -1,6 +1,7 @@
-#import pytest
-#import toga
-#from .probe import get_probe
+import pytest
+import toga
+from .probe import get_probe
+from ..tests_backend.proxies.box_proxy import BoxProxy
 
 """ TODO: Don't enable until below is implemented.
 @pytest.fixture
@@ -8,17 +9,12 @@ async def widget():
     raise NotImplementedError("test modules must define a `widget` fixture")
 """
 
-""" TODO: Implement. atm: manually in each widget test file.
+#sync ver
 @pytest.fixture
-async def probe(main_window, widget):
+def probe(main_window, widget):
     old_content = main_window.content
-
-    box = toga.Box(children=[widget])
+    box = BoxProxy(children=[widget])
     main_window.content = box
     probe = get_probe(widget)
-    await probe.redraw(f"\nConstructing {widget.__class__.__name__} probe")
-    probe.assert_container(box)
     yield probe
-
     main_window.content = old_content
-"""
--- testbed/tests/widgets/probe.py
+++ testbed/tests/widgets/probe.py
@@ -1,13 +1,7 @@
-
-""" 
-TODO: 
-Need to get this working with remote proxy classes. Otherwise manually.
-Requires proxy objects 'replacing' toga objects dynamically if web.
-
 from importlib import import_module
 
 def get_probe(widget):
-    name = type(widget).__name__
-    module = import_module(f"tests_backend.widgets.{name.lower()}")
-    return getattr(module, f"{name}Probe")(widget)
-"""
+    name = type(widget).__name__          # e.g. "ButtonProxy"
+    base = name.removesuffix("Proxy")     # -> "Button"
+    module = import_module(f"tests.tests_backend.widgets.{base.lower()}")  # -> tests/tests_backend/widgets/button.py
+    return getattr(module, f"{base}Probe")(widget)
